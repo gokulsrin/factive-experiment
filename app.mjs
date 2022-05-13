9812a7019bc71c2348eb9757f6417f4110cce5b7
gokulsrin <gokul.srinivasan.23@dartmouth.edu>
2022-05-13 11:06:23 -0400

    adding for heroku

--- app.mjs
+++ app.mjs
@@ -8,19 +8,19 @@ dotenv.config();
 
 // sequel connection 
 // I am hard coding in the information here instead of using the .env function because, for whatever reason, it leads to errors...
-var con = mysql.createConnection({
-    host: "database-1.cfahjwyzytn4.us-east-2.rds.amazonaws.com",
-    user: "phillab",
-    password: "N4WTHU7f3u2a9Bx",
-    database: "factive_experiment",
-    port: "3306",
-    // socketPath: '/var/run/mysqld/mysqld.sock',
-  });
-// try to connect
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to database!");
-});
+// var con = mysql.createConnection({
+//     host: "database-1.cfahjwyzytn4.us-east-2.rds.amazonaws.com",
+//     user: "phillab",
+//     password: "N4WTHU7f3u2a9Bx",
+//     database: "factive_experiment",
+//     port: "3306",
+//     // socketPath: '/var/run/mysqld/mysqld.sock',
+//   });
+// // try to connect
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected to database!");
+// });
   
 // express app
 const app = express();
@@ -34,7 +34,7 @@ app.use(express.static('public'));
 
 // if we receive a "get" request from the path "./", then we will send the message receiving
 app.get('/', (req, res) => {
-    console.log("in get");
+    // console.log("in get");
 }); 
 // console.log("Database_URL & Host_URL", process.env.DATABASE, process.env.HOST);
 // post sends information as well... 
@@ -48,15 +48,16 @@ app.post('/data', (req, res) => {
     //   else console.log("Data recorded");
     // });
     // console.log(req.body.data);
-    var data = parseData(req.body.data[0], req.body.data[1]);
-    for (var row of data){
-        const msg = row;
-        // const msg = "INSERT INTO information_table (id, context, *condition, question, answer, correct, rt) VALUES('s', 'school', 'false belief', 'something', 'zero', 'F', '199')";
-        con.query(msg, function (err, result) {
-        if (err) console.log(err);
-        else console.log("Data recorded");
-        });
-    }
+
+    // var data = parseData(req.body.data[0], req.body.data[1]);
+    // for (var row of data){
+    //     const msg = row;
+    //     // const msg = "INSERT INTO information_table (id, context, *condition, question, answer, correct, rt) VALUES('s', 'school', 'false belief', 'something', 'zero', 'F', '199')";
+    //     con.query(msg, function (err, result) {
+    //     if (err) console.log(err);
+    //     else console.log("Data recorded");
+    //     });
+    // }
     
 });
 
--- public/instruction/instruction.js
+++ public/instruction/instruction.js
@@ -8,9 +8,9 @@ var video_instructions = '<p style="font-size:18px; color:black">In the followin
 var pos_video_instructions='<p style="font-size:18px; color:black">You will now be presented several questions about the video you watched.<br>'
 +'Answer the question by pressing the corresponding button on the screen. </p>'; 
 
-var study_description = ' Study description';
+var pre_instruction_trial = '<p style="font-size:18px; color:black"> To get you accustomed to this set up, we will now do a brief training session. </br> <b> Please press continue when you are ready to proceed. </b></p>'
 
-var practice_trial_instructions = '<p style="font-size:18px; color:black"> In this experiment you will watch a series of videos and answer 3 yes/no questions after each video. You will watch every video once, starting each by manually pressing play. The questions will be auto-paced and we ask that you respond to each as quickly as possible. To quickly respond, place your fingers over the ‘F’ and ‘J’ keys. F indicates a yes response. J indicates no response. In between each question you will be shown a plus sign to indicate the next question is about to appear.<br><br> <b> Please click continue. </b>';
+var general_instructions = '<p style="font-size:18px; color:black"> In this experiment you will watch a series of videos and answer 3 yes/no questions after each video. You will watch every video once, starting each by manually pressing play. The questions will be auto-paced and we ask that you respond to each as quickly as possible. To quickly respond, place your fingers over the ‘F’ and ‘J’ keys. F indicates a yes response. J indicates no response. In between each question you will be shown a plus sign to indicate the next question is about to appear.<br><br> <b> Please click continue. </b>';
 var post_video_instructions = '<p style="font-size:18px; color:black; margin: 50px;"> You will now be shown a series of yes no questions. Please place your index fingers on F and J. Press F for ‘Yes’ and J for ‘No’. Answer as quickly as possible. <br/> Remember the questions will be auto-paced. If you do not answer in the allotted [15] seconds, you will not have an opportunity to re-answer. </br> <b> Press the spacebar when you are ready to proceed. </b>';
 // p = yes, q = no
 var instruction_trial = {
